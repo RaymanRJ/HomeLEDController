e3102dad06fdedf1506524d7023a3663a2e2c4dd
Rayman Jamal <RaymanRJ@Live.com>
2022-04-23 15:53:48 -0400

    Moar

--- src/front-end/home-led-controller/src/components/home.js
+++ src/front-end/home-led-controller/src/components/home.js
@@ -3,13 +3,26 @@ import { Tiles } from '@rebass/layout'
 import Cabinet from './cabinet';
 
 export default class Home extends Component {
+    constructor(props){
+        super(props)
+
+        this.cabinets=[
+            <Cabinet id="OUTER_LEFT"/>,
+            <Cabinet id="INNER_LEFT"/>,
+            <Cabinet id="INNER_RIGHT"/>,
+            <Cabinet id="OUTER_RIGHT"/>
+        ]
+    }
     render (){
         return(
             <Tiles columns={[2, null, 4]}>
-                <Cabinet id="OUTER_LEFT"/>
-                <Cabinet id="INNER_LEFT"/>
-                <Cabinet id="INNER_RIGHT"/>
-                <Cabinet id="OUTER_RIGHT"/>
+                {this.cabinets.map((cabinet) => {
+                    return(
+                        <div>
+                            {cabinet}
+                        </div>
+                    )
+                })}
             </Tiles>
         )
     }
